ddb327ab19b29c64c1752cef26c460ca0b7064ec
Ken Zalewski <ken.zalewski@gmail.com>
2026-06-16 01:33:01 -0400

    Patch to openssl-1.1.1zh.  This version addresses five vulnerabilities:  CVE-2026-34180, CVE-2026-42766, CVE-2026-45447, CVE-2026-7383, and CVE-2026-9076

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -39,8 +39,8 @@ extern "C" {
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-# define OPENSSL_VERSION_NUMBER  0x1010120fL
-# define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.1zg  7 Apr 2026"
+# define OPENSSL_VERSION_NUMBER  0x1010121fL
+# define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.1zh  9 Jun 2026"
 
 /*-
  * The macros below are to be used for shared library (.so, .dll, ...)
